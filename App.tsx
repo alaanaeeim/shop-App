7ae7a879b2fe0d8ca0ebf038c0ef4f25402543f8
Naeeim <a.naeeim@soum.sa>
2025-08-17 17:38:12 +0300

    add env files and schemas

--- App.tsx
+++ App.tsx
@@ -5,9 +5,9 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { useEffect } from 'react';
 import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import Config from 'react-native-config';
 import SplashScreen from 'react-native-splash-screen';
 
 function App() {
@@ -22,6 +22,7 @@ function App() {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{fontFamily: 'ComicNeue-Bold', fontSize: 22}}>Welcome to the React Native App!</Text>
+        <Text>Config.BASE_URL - { Config.ENVIRONMENT }</Text>
       </View>
     </View >
   );
